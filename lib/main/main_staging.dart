8588f8f132f19fd054153e223db1a8a53320a074
Levent Kantaroğlu <63751824+leventkantaroglu@users.noreply.github.com>
2023-05-14 14:27:26 +0300

    Update main_staging.dart

--- lib/main/main_staging.dart
+++ lib/main/main_staging.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_project/main.dart';
 import 'package:flutter_project/main/app_env.dart';
 
-Future<void> main() async => mainCommon(AppEnvironment.DEV);
+Future<void> main() async => mainCommon(AppEnvironment.STAGING);
